a794845b5bb42526779db1c547a4d4fd012bfa83
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-20 12:39:26 +0530

    removed

--- backend/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize("booking", "root", "W@2915djkq#", {
-  dialect: "mysql",
-  host: "localhost",
-
-});
-
-module.exports = sequelize;
